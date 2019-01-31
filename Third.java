c9685f0fb833ad5cf5c89cead049f1628f813920
miketully426 <mike.m.tully@gmail.com>
2019-01-30 18:51:37 -0600

    Add files via upload

---
+++ Third.java
@@ -0,0 +1,12 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+
+public class Third {
+
+    public static void main(String[] args) {
+        System.out.println();
+        Scanner keyboard = new Scanner(System.in);
+    }
+}
